ef54a586d17efc62956d00a983a2cc5153200a91
Vahid B. Zadeh <vahid@trainwithpush.com>
2018-09-10 10:57:50 -0400

    Changed the image pixel ranges to get more accurate results. The box is being detected now.

--- DepthCameraLocalization.R
+++ DepthCameraLocalization.R
@@ -13,19 +13,18 @@ y_r = y_max - y_min;
 # Get the x and y of the object and the range of values
 x_p = 339;
 y_p = 360;
-R_x = 500;
-R_y = 500;
+R_x = 460;
+R_y = 460;
 # Convert the x and y coordinates to point cloud coordinates
 x = (x_r/R_x)*x_p + x_min;
 y = (y_r/R_y)*y_p + y_min;
-x = 0;
 ################ Data Cleaning ############
 # Remove points that are too far
 farPoints = which(abs(data[,3]) > 1.0);
 data = data[-farPoints,];
 ################ Analysis #################
 # Define a tolerance of 5 cm
-kTolerance = 0.05;
+kTolerance = 0.01;
 # Find all the points in the vicinity of the x and y point
 surfacePoints = data %>%
                 subset(abs(data[,1] - x) < kTolerance &
@@ -36,12 +35,7 @@ scatter3D(data[,1], data[,2], data[,3],
           pch = 18, ticktype = "detailed");
 
 scatter3D(surfacePoints[,1], surfacePoints[,2], surfacePoints[,3],
-          col='red',
+          col='red', add = FALSE,
           pch = 19, ticktype = "detailed");
 
 plotrgl()
-
-rect3D(x0 = 0, y0 = 0.5, z0 = 0, x1 = 1, z1 = 5, 
-       ylim = c(0, 1), bty = "g", facets = TRUE, 
-       border = "red", col ="#7570B3", alpha=0.5,
-       lwd = 2, phi = 20)
